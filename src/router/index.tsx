40915f94f846776da789df8ce007e7b8b1e94b46
Divine Anum <tpandivine48@gmail.com>
2024-01-23 01:13:10 +0000

    Initial commit

---
+++ src/router/index.tsx
@@ -0,0 +1,28 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+  ScrollRestoration,
+} from "react-router-dom";
+
+import { Home } from "@pages/Home";
+
+
+const Root = () => {
+  return (
+    <>
+      <ScrollRestoration />
+      <Outlet />
+    </>
+  );
+};
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<Home />} />
+      <Route path="*" element={<h1> Oops! This page does not exist</h1>} />
+    </Route>
+  )
+);
